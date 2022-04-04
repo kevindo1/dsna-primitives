5e05a01092811eccd638eaa8caabcf01f2d8b20e
Kevin Do <kevind0501@gmail.com>
2022-04-04 11:49:45 -0700

    moved fourth challenge to index.js

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function titleCase(sentence) {
 }
 
 console.log(titleCase('alchemy ROCKS goLD'));
+
+function at(arr, index) {
+  if (index > -1) {
+    return arr[index];
+  } else {
+    return arr[arr.length + index];
+  }
+}
+
+console.log(at(['a', 'b', 'c', 'd', 'e'], 1));
